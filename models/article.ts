736a9adf3363034869b286c3b3ceefd3e24cd875
pablo sinivassin <pablo.sinivassin@agregio-solutions.com>
2024-02-24 18:38:19 +0100

    :tada: Installing and creating model for article
    _

---
+++ models/article.ts
@@ -0,0 +1,23 @@
+export enum UrlsTypes {
+  WEBSITE = "website",
+  VIDEOS = "videos",
+  AUDIO = "audio",
+  SOCIAL = "social",
+  IMAGE = "image",
+}
+
+export interface Article {
+  title: string;
+  introduction: string | null;
+  main: string;
+  mainAudioUrl: string | null;
+  urlToMainIllustration: string;
+  publishedAt: string | null;
+  createAt: string;
+  updateAt: string;
+  author: string;
+  urls: {
+    type: UrlsTypes;
+    url: string;
+  }[];
+}
