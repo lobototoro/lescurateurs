9fea22ec15d6627c4d13eb9d1fb484a053e55474
pablo sinivassin <pablo.sinivassin@agregio-solutions.com>
2024-11-04 17:46:06 +0100

    :arrow_up: Upgrading nextjs and replacing sakura.css
    adding bulma css

--- app/article/[slug]/page.tsx
+++ app/article/[slug]/page.tsx
@@ -2,8 +2,9 @@ import { Article } from '@/models/article';
 import { getArticle } from '@/libs/articles';
 import { Params } from '@/models/slugs';
 
-export default async function ArticlePage({ params }: { params: Params }) {
-  const article = await getArticle(params.slug) as Article; 
+export default async function ArticlePage(props: { params: Promise<Params> }) {
+  const params = await props.params;
+  const article = await getArticle(params.slug) as Article;
 
   return (
     <>
