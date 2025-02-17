924dfac5b5f564df0f464b1a6cdf49be36d0e16a
pablo sinivassin <pablo.sinivassin@agregio-solutions.com>
2025-02-17 18:05:18 +0100

    adding color

--- app/components/ArticleTitle.tsx
+++ app/components/ArticleTitle.tsx
@@ -1,11 +1,12 @@
 import DOMPurify from 'isomorphic-dompurify';
 
-export const ArticleTitle = ({ text, level, size = 'medium', }: { 
+export const ArticleTitle = ({ text, level, size = 'medium', color = 'white' }: { 
   text: string,
   level: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6',
+  color?: 'white' | 'black' | 'primary' | 'secondary',
   size?: 'small' | 'medium' | 'large' | 'extra-large'
 }) => {
-  const tag: string = DOMPurify.sanitize(`<${level} class="article-title_${size}">${text}</${level}>`);
+  const tag: string = DOMPurify.sanitize(`<${level} class="article-title_${size} article-title_${color}">${text}</${level}>`);
 
   return (
     <div dangerouslySetInnerHTML={{ __html: tag }} />
