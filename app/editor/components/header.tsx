729c37fb09f594c16dc5fff099babd26daae87bc
pablo sinivassin <pablo.sinivassin@agregio-solutions.com>
2025-02-25 11:40:01 +0100

    :sparkles: Creating form structure for editor page
    adding a header componenent for the form

--- app/editor/components/header.tsx
+++ app/editor/components/header.tsx
@@ -1,19 +1,28 @@
-/* eslint-disable @next/next/no-html-link-for-pages */
-"use client";
-import { useUser } from "@auth0/nextjs-auth0/client";
-
-export default function Header() {
-  const { user } = useUser();
+export default function Header({ role, permissions }: {
+  role: string;
+  permissions: string;
+}) {
+  const stringifiedPermissions = JSON.parse(permissions);
+  const filteredMenu = stringifiedPermissions.map( (permission: string) => {
+    if (permission.split(':')[1] === 'articles') {
+      return `${permission.split(':')[0]} article`;
+    }
+    if (permission.split(':')[1] === 'user') {
+      return `${permission.split(':')[0]} user`;
+    }
+    if (permission.split(':')[1] === 'maintenance') {
+      return `${permission.split(':')[0]} maintenance`;
+    }
+  });
 
   return (
     <header>
-      { user && (
-        <>
-          <a href="/api/auth/logout" title='logout'>
-            Logout
-          </a>
-        </>
-      ) }
+      <ul>
+        <li>Role: { role }</li>
+        { filteredMenu.map( (permission: string, index: number) => (
+          <li key={ index }>{ permission }</li>
+        ))}
+      </ul>
     </header>
   );
 }
--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,5 +1,8 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
 import { auth0 } from "@/lib/auth0"
+import { getUser } from "@/lib/users";
+import { User } from "@/models/user";
+import Header from "./components/header";
 
 export default async function Login() {
   const session = await auth0.getSession()
@@ -13,10 +16,15 @@ export default async function Login() {
       </>
     );
   }
-  
+
+  const credentials = await getUser(session?.user.email as string) as User;
+
   return (
     <>
-      You're logged in {session?.user.name}!
+      <Header
+        role={credentials.role}
+        permissions={credentials.permissions}
+      />
       <br />
       <a href="/auth/logout">
         <button>Log out</button>
