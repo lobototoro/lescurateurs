5f57898d6cb8cf245e7ad4c5eb0dca82d6873b8a
pablo sinivassin <pablo.sinivassin@agregio-solutions.com>
2025-03-11 12:33:26 +0100

    finishing create article form - phase 1

--- app/editor/components/formComponents/createArticlesForm.tsx
+++ app/editor/components/formComponents/createArticlesForm.tsx
@@ -1,8 +1,8 @@
 "use client";
-import { useActionState, startTransition, useCallback, useEffect } from "react";
+import { useActionState, useEffect, startTransition, useRef } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import{ isValid, z } from "zod"; 
+import{ z } from "zod"; 
 
 import { isEmpty } from "@/lib/isEmpty";
 import { articleSchema } from "@/models/articleSchema";
@@ -12,7 +12,7 @@ import { UrlsTypes } from '@/models/article';
 import { ArticleTitle } from "@/app/components/single-elements/ArticleTitle";
 
 export default function CreateArticleForm() {
-  const [state, formAction, isPending] = useActionState(createArticleAction, null);
+  const [state, formAction] = useActionState(createArticleAction, null);
   
   // init for react-hook-form where we define methods that will be used and the engine for validation (zod) and the default values
   const {
@@ -40,9 +40,18 @@ export default function CreateArticleForm() {
   register('urls');
   const urlsToArray = getValues('urls') !== '' ? JSON.parse(getValues('urls')) : [];
 
-  const onSubmit = useCallback((data: z.infer<typeof articleSchema>) => {
+  // utilities for the modal, that appears when form is sent successfully or with errors
+  const formSentModal = useRef<HTMLDivElement>(null);
+  const openModal = () => formSentModal.current?.classList.add('is-active'); const closeModal = () => {
+    if (state?.message) {
+      reset();
+    }
+    formSentModal.current?.classList.remove('is-active');
+  };
 
-    startTransition(async () => {
+  // onSubmit that connects useForm with server action
+  const onSubmit = (data: z.infer<typeof articleSchema>) => {
+    startTransition(() => {
       const formData = new FormData();
       formData.append('title', data.title);
       formData.append('introduction', data.introduction);
@@ -50,9 +59,10 @@ export default function CreateArticleForm() {
       formData.append('urls', data.urls);
       formData.append('mainAudioUrl', data.mainAudioUrl || '');
       formData.append('urlToMainIllustration', data.urlToMainIllustration);
-      await formAction(formData);
+      formAction(formData);
+      openModal();
     });
-  }, [formAction]);
+  };
 
   // this useEffect aims to watch for errors while typing
   useEffect(() => {
@@ -61,12 +71,9 @@ export default function CreateArticleForm() {
         clearErrors(name);
       }
     });
-    if (state?.message) {
-      reset();
-    }
 
     return () => subscription.unsubscribe();
-  }, [watch, clearErrors, state, reset]);
+  }, [watch, clearErrors]);
 
   const initialUrls = {
       type: 'website' as UrlsTypes,
@@ -95,32 +102,31 @@ export default function CreateArticleForm() {
   
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      { state && <p className="has-text-success">{state.text}</p>}
       <div className="mt-6">
         <div className="field">
         <label className="label" aria-label="label du champ Titre" htmlFor="title">Titre</label>
         <div className="field">
-          <input className="input" type="text" {...register('title')} />
+          <input className="input" type="text" {...register('title')} required />
         </div>
         { errors.title && <p className="has-text-danger">{errors.title.message}</p>}
       </div>
 
       <div className="field">
         <label className="label" aria-label="label du champ introduction" htmlFor="introduction">introduction</label>
-        <textarea className="textarea" {...register('introduction')} rows={5}></textarea>
+        <textarea className="textarea" {...register('introduction')} rows={5} required></textarea>
         { errors.introduction && <p className="has-text-danger">{errors.introduction.message}</p>}
       </div>
 
       <div className="field">
         <label className="label" aria-label="label du champ Texte" htmlFor="main">Texte</label>
-        <textarea className="textarea" {...register('main')} rows={10}></textarea>
+        <textarea className="textarea" {...register('main')} rows={10} required></textarea>
         { errors.main && <p className="has-text-danger">{errors.main.message}</p>}
       </div>
 
       <div className="field">
         <label className="label is-inline-flex" aria-label="label du champ lien vers l'illustration" htmlFor="urlToMainIllustration">lien vers l'illustration</label>
         <div className="control">
-          <input className="input" type="url" {...register('urlToMainIllustration')} />
+          <input className="input" type="url" {...register('urlToMainIllustration')} required />
         </div>
         { errors.urlToMainIllustration && <p className="has-text-danger">{errors.urlToMainIllustration.message}</p>}
       </div>
@@ -128,7 +134,7 @@ export default function CreateArticleForm() {
       <div className="field">
         <label className="label" aria-label="label du champ lien audio principal" htmlFor="mainAudioUrl">lien audio principal</label>
         <div className="control">
-          <input className="input" type="url" {...register('mainAudioUrl')} />
+          <input className="input" type="url" {...register('mainAudioUrl')} required />
         </div>
         { errors.mainAudioUrl && <p className="has-text-danger">{errors.mainAudioUrl.message}</p>}
       </div>
@@ -150,9 +156,17 @@ export default function CreateArticleForm() {
     </div>
     
     { !isEmpty(errors) && <p className="has-text-danger">Des erreurs existent dans le formulaire</p>}
-    { state && <p className="mt-5 has-text-success">{state.text}</p>}
+    
+    <div className="modal" ref={formSentModal}>
+      <div className="modal-background"></div>
+      <div className={state?.message ? 'modal-content is-success' : 'modal-content is-danger'}>
+        <p className="is-size-6 has-text-white has-background-primary p-6">{state?.text}</p>
+        <button className="modal-close is-large" aria-label="close modal" onClick={() => closeModal()}></button>
+      </div>
+    </div>
+
     <div className="field mt-5">
-      <input type="submit" className="button is-primary is-size-6 has-text-white" value="Valider" disabled={!isValid || isPending} />
+      <input type="submit" className="button is-primary is-size-6 has-text-white" value="Valider" disabled={!isEmpty(errors)} />
     </div>
   </form>
 )};
