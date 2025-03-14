8ac1676862db7abcec586ff38850702317ccf1f4
pablo sinivassin <pablo.sinivassin@agregio-solutions.com>
2025-03-14 12:48:46 +0100

    update for articleTitle comp

--- app/components/single-elements/ArticleTitle.tsx
+++ app/components/single-elements/ArticleTitle.tsx
@@ -11,16 +11,26 @@ export const ArticleTitle = ({
   level,
   size = 'medium',
   color = 'white',
-  spacings = ''
+  spacings = '',
+  link,
 }: { 
   text: string,
   level: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6',
   color?: 'white' | 'black' | 'primary' | 'secondary',
   size?: 'small' | 'medium' | 'large' | 'extra-large',
-  spacings?: string | ''
+  spacings?: string | '',
+  link?: string,
 }) => {
   const tag: string = DOMPurify.sanitize(`<${level} class="${alegreya.className} article-title_${size} article-title_${color} ${spacings}">${text}</${level}>`);
 
+  if (link) {
+    const urlLink = DOMPurify.sanitize(`<a href="${link}" target="_blank" class="is-primary">${tag}</a>`);
+    
+    return (
+      <div dangerouslySetInnerHTML={{ __html: urlLink }} />  
+    )
+  }
+ 
   return (
     <div dangerouslySetInnerHTML={{ __html: tag }} />
   );
--- test-utils/ArticleTitle.test.tsx
+++ test-utils/ArticleTitle.test.tsx
@@ -1,15 +1,7 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import { vi, expect, test } from 'vitest';
 
 import { ArticleTitle } from '../app/components/single-elements/ArticleTitle';
-
-// vitest.mock('isomorphic-dompurify', () => ({
-//   default: vitest.fn().mockImplementation(() => {
-//     return {
-//       sanitize: vitest.fn().mockImplementation((html) => html),
-//     };
-//   })
-// }));
 
 vi.mock('next/font/google', () => ({
   Alegreya: () => ({
@@ -25,4 +17,14 @@ test('ArticleTitle', () => {
   const container = screen.getByText('Article title');
   expect(container).toBeDefined();
   expect(container.className).toBe('undefined article-title_extra-large article-title_white');
+});
+
+test('articleTitle with link', () => {
+  cleanup();
+  render(<ArticleTitle text="Article title" level="h1" size="extra-large" color="white" link="/article/slug" />);
+  
+  const link = document.querySelector('a');
+  expect(link?.classList[0]).toBe('is-primary');
+  expect(link?.tagName).toBe('A');
+  expect(link?.getAttribute('href')).toBe('/article/slug');
 });
