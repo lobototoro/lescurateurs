6274234435ddae6927223fd28b41e6584b55cd9e
pablo <lobototoro@users.noreply.github.com>
2025-03-20 10:50:24 +0100

    :recycle: Reusable form markup for article (#16)
    reusable form comp for create and update article

--- app/editor/components/formComponents/createArticlesForm.tsx
+++ app/editor/components/formComponents/createArticlesForm.tsx
@@ -4,12 +4,10 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import{ z } from "zod"; 
 
-import { isEmpty } from "@/lib/isEmpty";
 import { articleSchema } from "@/models/articleSchema";
 import { createArticleAction } from "@/app/articleActions";
-import { AddUrlsObjects } from "./addUrlsObjects";
 import { UrlsTypes } from '@/models/article';
-import { ArticleTitle } from "@/app/components/single-elements/ArticleTitle";
+import ArticleMarkupForm from "@/app/components/articleHTMLForm";
 
 export default function CreateArticleForm() {
   const [state, formAction] = useActionState(createArticleAction, null);
@@ -101,72 +99,16 @@ export default function CreateArticleForm() {
   };
   
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="mt-6">
-        <div className="field">
-        <label className="label" aria-label="label du champ Titre" htmlFor="title">Titre</label>
-        <div className="field">
-          <input className="input" type="text" {...register('title')} data-testid="title" required />
-        </div>
-        { errors.title && <p className="has-text-danger">{errors.title.message}</p>}
-      </div>
-
-      <div className="field">
-        <label className="label" aria-label="label du champ introduction" htmlFor="introduction">introduction</label>
-        <textarea className="textarea" {...register('introduction')} rows={5} data-testid="introduction" required></textarea>
-        { errors.introduction && <p className="has-text-danger">{errors.introduction.message}</p>}
-      </div>
-
-      <div className="field">
-        <label className="label" aria-label="label du champ Texte" htmlFor="main">Texte</label>
-        <textarea className="textarea" {...register('main')} rows={10} data-testid="main" required></textarea>
-        { errors.main && <p className="has-text-danger">{errors.main.message}</p>}
-      </div>
-
-      <div className="field">
-        <label className="label is-inline-flex" aria-label="label du champ lien vers l'illustration" htmlFor="urlToMainIllustration">lien vers l'illustration</label>
-        <div className="control">
-          <input className="input" type="url" {...register('urlToMainIllustration')} data-testid="urlToMainIllustration" required />
-        </div>
-        { errors.urlToMainIllustration && <p className="has-text-danger">{errors.urlToMainIllustration.message}</p>}
-      </div>
-
-      <div className="field">
-        <label className="label" aria-label="label du champ lien audio principal" htmlFor="mainAudioUrl">lien audio principal</label>
-        <div className="control">
-          <input className="input" type="url" {...register('mainAudioUrl')} data-testid="mainAudioUrl" required />
-        </div>
-        { errors.mainAudioUrl && <p className="has-text-danger">{errors.mainAudioUrl.message}</p>}
-      </div>
-
-      <ArticleTitle
-        text="Ajouter des liens multimédias"
-        level="h4"
-        size="medium"
-        color="white"
-        spacings="mt-5 mb-4"
-      />
-      { errors.urls && <p className="has-text-danger">{errors.urls.message}</p>}
-      <AddUrlsObjects
-        urls={urlsToArray}
-        updateUrls={updateUrls}
-        addInputs={addInputs}
-        removeInputs={removeInputs}
-      />
-    </div>
-    
-    { !isEmpty(errors) && <p className="has-text-danger">Des erreurs existent dans le formulaire</p>}
-    
-    <div className="modal" ref={formSentModal} data-testid="create-article-modal">
-      <div className="modal-background"></div>
-      <div className={state?.message ? 'modal-content is-success' : 'modal-content is-danger'}>
-        <p className="is-size-6 has-text-white has-background-primary p-6">{state?.text}</p>
-        <button className="modal-close is-large" aria-label="close modal" onClick={() => closeModal()}></button>
-      </div>
-    </div>
-
-    <div className="field mt-5">
-      <input type="submit" className="button is-primary is-size-6 has-text-white" value="Valider" data-testid="submit" disabled={!isEmpty(errors)} />
-    </div>
-  </form>
+  <ArticleMarkupForm
+    handleSubmit={handleSubmit(onSubmit)}
+    register={register}
+    errors={errors}
+    urlsToArray={urlsToArray}
+    updateUrls={updateUrls}
+    addInputs={addInputs}
+    removeInputs={removeInputs}
+    formSentModal={formSentModal as React.RefObject<HTMLDivElement>}
+    state={state}
+    closeModal={closeModal}
+  />
 )};
