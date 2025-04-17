c099c3a72f25173936855b2408098ef080f632a9
pablo sinivassin <psinivassin@gmail.com>
2025-04-17 15:21:15 +0200

    removing playwright coverage by istanbul

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
-import IstanbulPlugin from 'vite-plugin-istanbul';
 
 export default defineConfig({
   plugins: [
-    react(),
-    ...(process.env.USE_BABEL_PLUGIN_ISTANBUL
-      ? [
-          IstanbulPlugin({
-            include: './',
-            exclude: ['node_modules', 'tests-e2e/**'],
-            extension: ['.js', '.ts', '.tsx'],
-          }),
-        ]
-      : []),
-  ],
+    react()],
   test: {
     environment: 'jsdom',
     setupFiles: ['vitest-setup.ts'],
