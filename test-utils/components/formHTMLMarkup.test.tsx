53e0f68fb3613274d11ba727f80c7932f76fe4aa
pablo sinivassin <psinivassin@gmail.com>
2025-09-19 17:57:30 +0200

    :white_check_mark: Passing test for formHTMLMarkup file
    there is a problem with update article test that fails one on two times

--- test-utils/components/formHTMLMarkup.test.tsx
+++ test-utils/components/formHTMLMarkup.test.tsx
@@ -2,8 +2,7 @@ import ArticleMarkupForm from "@/app/components/single-elements/articleHTMLForm"
 import { fireEvent, render } from "@testing-library/react";
 import { expect, it, vi } from "vitest";
 
-it('should render correct markup', () => {
-  const mockProps = {
+const mockProps = {
     handleSubmit: vi.fn(),
     register: vi.fn(),
     errors: {},
@@ -18,53 +17,36 @@ it('should render correct markup', () => {
     state: { message: false, text: '' },
     closeModal: vi.fn(),
     target: 'create',
+    getMainContent: vi.fn(),
+    setMainContent: vi.fn(),
   };
 
+it('should render correct markup', () => {
   const { asFragment } = render(<ArticleMarkupForm {...mockProps} />);
   expect(asFragment()).toMatchSnapshot();
 });
 
 it('should render correct markup for update', () => {
-  const mockProps = {
-    handleSubmit: vi.fn(),
-    register: vi.fn(),
-    errors: {},
-    urlsToArray: [],
-    updateUrls: vi.fn(),
-    removeUrl: vi.fn(),
-    addUrl: vi.fn(),
-    handleChange: vi.fn(),
-    addInputs: vi.fn(),
-    removeInputs: vi.fn(),
-    formSentModal: null,
-    state: { message: false, text: '' },
-    closeModal: vi.fn(),
+  const mockPropsUpdate = {
+    ...mockProps,
     target: 'update',
   };
 
-  const { asFragment, getByTestId } = render(<ArticleMarkupForm {...mockProps} />);
+  const { asFragment, getByTestId } = render(
+    <ArticleMarkupForm {...mockPropsUpdate} />
+  );
   expect(asFragment()).toMatchSnapshot();
   expect(getByTestId('title')).toHaveProperty('disabled', true);
 });
 
 it('Should display an additionnal link line with blinking validate button while modified', () => {
-  const mockProps = {
-    handleSubmit: vi.fn(),
-    register: vi.fn(),
-    errors: {},
+  const mockPropsAddUrl = {
+    ...mockProps,
     urlsToArray: ['type: website, url: https://example.com, credits: test'],
-    updateUrls: vi.fn(),
-    removeUrl: vi.fn(),
-    addUrl: vi.fn(),
-    handleChange: vi.fn(),
-    addInputs: vi.fn(),
-    removeInputs: vi.fn(),
-    formSentModal: null,
     state: { message: true, text: 'TEST' },
-    closeModal: vi.fn(),
     target: 'update',
   };
-  const { getByTestId } = render(<ArticleMarkupForm {...mockProps} />);
+  const { getByTestId } = render(<ArticleMarkupForm {...mockPropsAddUrl} />);
   expect(getByTestId('select-type')).toBeDefined();
   fireEvent.change(getByTestId('select-type'), { target: { value: 'videos' } });
   expect(getByTestId('add-url-button')?.classList.toString()).toMatch(/blink/);
