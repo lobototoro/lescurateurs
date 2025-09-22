177390e962dcdfcab6eb57c5e61c4b41c905c5f8
pablo <lobototoro@users.noreply.github.com>
2025-09-22 19:13:29 +0200

    🔫 updating 'updatedAt' field in manageArticle (#44)

--- lib/articles.ts
+++ lib/articles.ts
@@ -1,4 +1,4 @@
-"use server";
+'use server';
 import sql from 'better-sqlite3';
 import { cache } from 'react';
 
@@ -11,30 +11,30 @@ import { Slugs } from '@/models/slugs';
 const db = sql('lcfr.db');
 
 export const getArticle = cache(async (slug: string) => {
-  return db
-    .prepare('SELECT * FROM articles WHERE slug = ?')
-    .get(slug);
+  return db.prepare('SELECT * FROM articles WHERE slug = ?').get(slug);
 });
 
-export const getArticleById = cache(async (id: number | bigint) =>{
-  return db
-    .prepare('SELECT * FROM articles WHERE id =?')
-    .get(id);
+export const getArticleById = cache(async (id: number | bigint) => {
+  return db.prepare('SELECT * FROM articles WHERE id =?').get(id);
 });
 
 export const getSlugs = cache(async () => {
-  return db
-    .prepare('SELECT * FROM slugs')
-    .all();
+  return db.prepare('SELECT * FROM slugs').all();
 });
 
 export const createSlug = cache(async (slugObject: Slugs) => {
-  return db.prepare('INSERT INTO slugs (slug, createdAt, articleId) VALUES (@slug, @createdAt, @articleId)')
+  return db
+    .prepare(
+      'INSERT INTO slugs (slug, createdAt, articleId) VALUES (@slug, @createdAt, @articleId)'
+    )
     .run(slugObject);
 });
 
 export const createArticle = cache(async (article: Article) => {
-  return db.prepare('INSERT INTO articles (slug, title, introduction, main, urls, mainAudioUrl, urlToMainIllustration, author, author_email, createdAt, updatedAt, publishedAt, validated, shipped) VALUES (@slug, @title, @introduction, @main, @urls, @mainAudioUrl, @urlToMainIllustration, @author, @author_email, @createdAt, @updatedAt, @publishedAt, @validated, @shipped)') 
+  return db
+    .prepare(
+      'INSERT INTO articles (slug, title, introduction, main, urls, mainAudioUrl, urlToMainIllustration, author, author_email, createdAt, updatedAt, publishedAt, validated, shipped) VALUES (@slug, @title, @introduction, @main, @urls, @mainAudioUrl, @urlToMainIllustration, @author, @author_email, @createdAt, @updatedAt, @publishedAt, @validated, @shipped)'
+    )
     .run(article);
 });
 
@@ -50,35 +50,46 @@ export const searchArticles = cache(async (searchTerm: string) => {
     .all({ searchTerm: `%${searchTerm}%` });
 });
 
-export const deleteArticle = cache (async(articleId: number | bigint) => {
-  return db.prepare('DELETE FROM articles WHERE id = ?')
-    .run(articleId);
+export const deleteArticle = cache(async (articleId: number | bigint) => {
+  return db.prepare('DELETE FROM articles WHERE id = ?').run(articleId);
 });
 
 export const deleteSlug = cache(async (slugId: number | bigint) => {
-  return db.prepare('DELETE FROM slugs WHERE articleId = ?')
-    .run(slugId);
+  return db.prepare('DELETE FROM slugs WHERE articleId = ?').run(slugId);
 });
 
 export const updateArticle = cache(async (article: Article) => {
-  return db.prepare('UPDATE articles SET title = @title, introduction = @introduction, main = @main, urls = @urls, mainAudioUrl = @mainAudioUrl, urlToMainIllustration = @urlToMainIllustration, createdAt = @createdAt, updatedAt = @updatedAt, publishedAt = @publishedAt, validated = @validated, shipped = @shipped, author = @author, author_email = @author_email, slug = @slug WHERE id = @id')
-  .run(article);
+  return db
+    .prepare(
+      'UPDATE articles SET title = @title, introduction = @introduction, main = @main, urls = @urls, mainAudioUrl = @mainAudioUrl, urlToMainIllustration = @urlToMainIllustration, createdAt = @createdAt, updatedAt = @updatedAt, publishedAt = @publishedAt, validated = @validated, shipped = @shipped, author = @author, author_email = @author_email, slug = @slug WHERE id = @id'
+    )
+    .run(article);
 });
 
-export const validateArticle = cache(async (validateProps: {
-  articleId: number | bigint,
-  validatedValue: string
-}) => {
-
-  return db.prepare('UPDATE articles SET validated = @validatedValue WHERE id = @articleId')
-    .run(validateProps);
+export const validateArticle = cache(
+  async (validateProps: {
+    articleId: number | bigint;
+    validatedValue: string;
+    updatedAt: string;
+  }) => {
+    return db
+      .prepare(
+        'UPDATE articles SET validated = @validatedValue, updatedAt = @updatedAt WHERE id = @articleId'
+      )
+      .run(validateProps);
   }
 );
 
-export const shipArticle = cache(async (shipProps: {
-  articleId: number | bigint,
-  shippedValue: string
-}) => {
-  return db.prepare('UPDATE articles SET shipped = @shippedValue WHERE id = @articleId')
-    .run(shipProps);
-});
+export const shipArticle = cache(
+  async (shipProps: {
+    articleId: number | bigint;
+    shippedValue: string;
+    updatedAt: string;
+  }) => {
+    return db
+      .prepare(
+        'UPDATE articles SET shipped = @shippedValue, updatedAt = @updatedAt WHERE id = @articleId'
+      )
+      .run(shipProps);
+  }
+);
