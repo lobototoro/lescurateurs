194a233914fd1f5afe49c10f84ce7d6875e33f19
pablo sinivassin <psinivassin@gmail.com>
2025-09-26 17:59:59 +0200

    adding date-fns to manage date manipulation when displaying articles

--- app/article/[slug]/page.tsx
+++ app/article/[slug]/page.tsx
@@ -1,3 +1,6 @@
+import { fr } from 'date-fns/locale/fr';
+import { formatDistanceToNow } from 'date-fns';
+
 import { Article } from '@/models/article';
 import { getArticle } from '@/lib/articles';
 import { Params } from '@/models/slugs';
@@ -5,7 +8,13 @@ import { ArticleTitle } from '@/app/components/single-elements/ArticleTitle';
 
 export default async function ArticlePage(props: { params: Promise<Params> }) {
   const params = await props.params;
-  const article = await getArticle(params.slug) as Article;
+  const article = (await getArticle(params.slug)) as Article;
+  const publishedDate =
+    article.publishedAt &&
+    formatDistanceToNow(article.publishedAt || '', {
+      locale: fr,
+      addSuffix: true,
+    });
 
   return (
     <div className="container">
@@ -13,10 +22,11 @@ export default async function ArticlePage(props: { params: Promise<Params> }) {
         text={article.title}
         level="h2"
         size="extra-large"
-        color="black" />
+        color="black"
+      />
       <p>{article.introduction}</p>
       <p>{article.main}</p>
-      <h4>{article.publishedAt}</h4>
+      {publishedDate && <p>Publié {publishedDate}</p>}
       <h5>{article.author}</h5>
     </div>
   );
