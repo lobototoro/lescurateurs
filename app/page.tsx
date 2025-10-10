ef6d86463d41bc4132eb2c0a385e50eba6c87499
pablo sinivassin <psinivassin@gmail.com>
2025-10-10 17:03:19 +0200

    removing console.info in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,16 @@
 // import styles from "./page.module.css";
-import { getSlugs } from "@/lib/articles";
-import { Slugs } from "@/models/slugs";
-import { ArticleList } from "./components/ArticleList";
-import { Suspense } from "react";
+import { getSlugs } from '@/lib/articles';
+import { Slugs } from '@/models/slugs';
+import { ArticleList } from './components/ArticleList';
+import { Suspense } from 'react';
 
 export default function Home() {
   const Content = async () => {
-    const slugs = await getSlugs() as Slugs[];
-    console.info('Slugs fetched in Home page:', slugs);
+    const slugs = (await getSlugs()) as Slugs[];
 
     return <ArticleList list={slugs} />;
-  }
-  
+  };
+
   return (
     <section>
       <Suspense fallback={<p>Loading...</p>}>
